41bc7c0902b305f7c16427594dc3d920ec8135c2
Army <keymasterviriya1150@gmail.com>
2019-07-22 20:02:12 +0700

    add fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,51 +1,51 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import {Link, graphql} from "gatsby"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
-
+import {rhythm} from "../utils/typography"
+import {Helmet} from "react-helmet"
 class BlogIndex extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-    const posts = data.allMarkdownRemark.edges
+    render() {
+        const {data} = this.props
+        const siteTitle = data.site.siteMetadata.title
+        const posts = data.allMarkdownRemark.edges
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
-        <Bio />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </div>
-          )
-        })}
-      </Layout>
-    )
-  }
+        return (
+            <Layout location={this.props.location} title={siteTitle}>
+                <SEO title="All posts"/> {posts.map(({node}) => {
+                    const title = node.frontmatter.title || node.fields.slug
+                    return (
+                        <div key={node.fields.slug}>
+                            <h3
+                                style={{
+                                marginBottom: rhythm(1 / 4)
+                            }}>
+                                <Link
+                                    style={{
+                                    boxShadow: `none`
+                                }}
+                                    to={node.fields.slug}>
+                                    {title}
+                                </Link>
+                            </h3>
+                            <small>{node.frontmatter.date}</small>
+                            <p
+                                dangerouslySetInnerHTML={{
+                                __html: node.frontmatter.description || node.excerpt
+                            }}/>
+                        </div>
+                    )
+                })}
+            </Layout>
+        )
+    }
 }
 
 export default BlogIndex
 
-export const pageQuery = graphql`
+export const pageQuery = graphql `
   query {
     site {
       siteMetadata {
