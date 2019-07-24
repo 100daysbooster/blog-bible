e3d8a08b2537f4b69d8be1e881fd2ec4c61ac0be
Army <keymasterviriya1150@gmail.com>
2019-07-24 13:28:02 +0700

    add html  css for toggle

--- src/html.js
+++ src/html.js
@@ -12,6 +12,7 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
+          <link href="https://fonts.googleapis.com/css?family=Pridi&display=swap" rel="stylesheet"></link>
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes} className="light">
