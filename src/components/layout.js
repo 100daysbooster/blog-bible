7c3c1e248b4625f9f0d4446b49656a35f216b308
Army <keymasterviriya1150@gmail.com>
2019-07-24 11:46:56 +0700

    create toggle :tada:

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,70 +1,147 @@
-import React from "react"
-import {Link} from "gatsby"
-import {rhythm, scale} from "../utils/typography"
-import './index.css'
+import React from 'react';
+import { Link } from 'gatsby';
+import Toggle from './Toggle';
+import Helmet from 'react-helmet';
+
+import { rhythm, scale } from '../utils/typography';
+import sun from '../../content/assets/moon.png';
+import moon from '../../content/assets/moon.png';
+
 class Layout extends React.Component {
-    render() {
-        const {location, title, children} = this.props
-        const rootPath = `${__PATH_PREFIX__}/`
-        let header
-        
-        if (location.pathname === rootPath) {
-            header = (
-                <h1
-                    style={{
-                    ...scale(1.5),
-                    marginBottom: rhythm(1.5),
-                    marginTop: 0,
-                    fontFamily: `Pridi`
-                }}>
-                    <Link
-                        style={{
-                        boxShadow: `none`,
-                        textDecoration: `none`,
-                        color: `inherit`
-                    }}
-                        to={`/`}>
-                        {title}
-                    </Link>
-                </h1>
-            )
-        } else {
-            header = (
-                <h3
-                    style={{
-                    fontFamily: `Pridi`,
-                    marginTop: 0
-                }}>
-                    <Link
-                        style={{
-                        boxShadow: `none`,
-                        textDecoration: `none`,
-                        color: `inherit`
-                    }}
-                        to={`/`}>
-                        {title}
-                    </Link>
-                </h3>
-            )
-        }
-        return (
-            <div
-                style={{
-                marginLeft: `auto`,
-                marginRight: `auto`,
-                maxWidth: rhythm(24),
-                padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-                fontFamily: `Pridi`,
-            }}>
-                <header>{header}</header>
-                <main>{children}</main>
-                <footer>
-                    © {new Date().getFullYear()}, Built by {` `}
-                    <a href="https://www.facebook.com/codegeassmasterzeronebreakout">Army</a>
-                </footer>
-            </div>
-        )
+  state = {
+    theme: null,
+  };
+  componentDidMount() {
+    this.setState({ theme: window.__theme });
+    window.__onThemeChange = () => {
+      this.setState({ theme: window.__theme });
+    };
+  }
+  renderHeader() {
+    const { location, title } = this.props;
+    const rootPath = `${__PATH_PREFIX__}/`;
+
+    if (location.pathname === rootPath) {
+      return (
+        <h1
+          style={{
+            ...scale(0.75),
+            marginBottom: 0,
+            marginTop: 0,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: 'none',
+              textDecoration: 'none',
+              color: 'var(--textTitle)',
+            }}
+            to={'/'}
+          >
+            {title}
+          </Link>
+        </h1>
+      );
+    } else {
+      return (
+        <h3
+          style={{
+            fontFamily: 'Montserrat, sans-serif',
+            marginTop: 0,
+            marginBottom: 0,
+            height: 42, // because
+            lineHeight: '2.625rem',
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: 'none',
+              textDecoration: 'none',
+              color: 'rgb(255, 167, 196)',
+            }}
+            to={'/'}
+          >
+            {title}
+          </Link>
+        </h3>
+      );
     }
+  }
+  render() {
+    const { children } = this.props;
+
+    return (
+      <div
+        style={{
+          color: 'var(--textNormal)',
+          background: 'var(--bg)',
+          transition: 'color 0.2s ease-out, background 0.2s ease-out',
+          minHeight: '100vh',
+        }}
+      >
+        <Helmet
+          meta={[
+            {
+              name: 'theme-color',
+              content: this.state.theme === 'light' ? '#ffa8c5' : '#282c35',
+            },
+          ]}
+        />
+        <div
+          style={{
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            maxWidth: rhythm(24),
+            padding: `2.625rem ${rhythm(3 / 4)}`,
+          }}
+        >
+          <header
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              marginBottom: '2.625rem',
+            }}
+          >
+            {this.renderHeader()}
+            {this.state.theme !== null ? (
+              <Toggle
+                icons={{
+                  checked: (
+                    <img
+                      src={moon}
+                      width="16"
+                      height="16"
+                      role="presentation"
+                      style={{ pointerEvents: 'none' }}
+                    />
+                  ),
+                  unchecked: (
+                    <img
+                      src={sun}
+                      width="16"
+                      height="16"
+                      role="presentation"
+                      style={{ pointerEvents: 'none' }}
+                    />
+                  ),
+                }}
+                checked={this.state.theme === 'dark'}
+                onChange={e =>
+                  window.__setPreferredTheme(
+                    e.target.checked ? 'dark' : 'light'
+                  )
+                }
+              />
+            ) : (
+              <div style={{ height: '24px' }} />
+            )}
+          </header>
+          {children}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Layout
+export default Layout;
